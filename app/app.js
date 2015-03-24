d39d200bc29deeaf3e4c3609a3d58497b0c9fb14
Alex <almiddeleer@gmail.com>
2015-03-23 20:57:41 -0400

    feature: controllers module

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('myApp', ['ngRoute']);
+angular.module('myApp', ['myAppControllers','ngRoute']);
 
 angular.module('myApp')
    .controller('app-ctrl', function($scope) {
@@ -6,8 +6,22 @@ angular.module('myApp')
       $scope.aTool = "AngularJS";
    });
 
+angular.module('myAppControllers', []);
+angular.module('myAppControllers')
+   .controller('view1-ctrl', function($scope) {
+      $scope.whereAmI = 'I am in the other view';
+   });
+
 angular.module('myApp')
    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/',{
+         templateUrl: 'app/views/home.html',
+         controller: 'app-ctrl'
+      });
+      $routeProvider.when('/view1/',{
+         templateUrl: 'app/views/view.html',
+         controller: 'view1-ctrl'
+      });
       $routeProvider.otherwise({
          templateUrl: 'app/views/home.html',
          controller: 'app-ctrl'
