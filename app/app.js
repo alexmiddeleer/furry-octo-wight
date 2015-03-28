b41502c634e3cc675801090dd90545ebbae19cfb
Alex <almiddeleer@gmail.com>
2015-03-27 23:36:38 -0400

    initial commit. A checkerboard is visible

--- app/app.js
+++ app/app.js
@@ -5,39 +5,43 @@ angular.module('myApp')
       home: {
          text:'Home',
          href:'#'
-      },
-      view: {
-         text:'A thing',
-         href:'#/view/'
-   }});
+      }
+   })
+   .constant('boardSize', 8)
+;
 
 angular.module('myApp')
    .controller('appCtrl', function($scope, myAppLinks) {
       $scope.bannerLinks = myAppLinks;
-   });
-
-angular.module('myAppControllers', []);
-angular.module('myAppControllers')
-   .controller('view1Ctrl', function($scope) {
-      $scope.aThing = "something";
-      $scope.aTool = "AngularJS, Bootstrap, and Lodash";
    })
-   .controller('view2Ctrl', function($scope) {
-      $scope.whereAmI = 'I am in the other view';
-   })
+;
 
 angular.module('myApp')
    .config(['$routeProvider', function($routeProvider) {
-      $routeProvider.when( '', {
-         templateUrl: 'app/views/home.html',
-         controller: 'view1Ctrl'
-      });
-      $routeProvider.when( '/view' ,{
-         templateUrl: 'app/views/view.html',
-         controller: 'view2Ctrl'
-      });
       $routeProvider.otherwise({
          templateUrl: 'app/views/home.html',
-         controller: 'view1Ctrl'
+         controller: 'viewCtrl'
       });
-   }]);
+   }])
+;
+
+angular.module('myAppControllers', []);
+angular.module('myAppControllers')
+   .controller('viewCtrl', function($scope, boardSize, $sce) {
+      var grid = [];
+      for (var i = 0; i < boardSize; i++) {
+         grid.push([]);
+         for (var j = 0; j < boardSize; j++) {
+            var k = ( i % 2 === 0 ? 0 : 1 );
+            var white = (((i*boardSize)+j+k)%2 === 0);
+            grid[i].push({
+               symbol:  white ? '\u25A0' :'\u25A1' 
+            });
+         }
+      }
+      $scope.grid = grid;
+      $scope.getSymbol = function(square) {
+         return $sce.trustAsHtml( square );
+      };
+   })
+
