8ad1d1d6c5abaa3302a068503f5ce06feebaf345
Alex Middeleer <almiddeleer@gmail.com>
2015-03-30 09:27:46 -0400

    Revert "starting to work on game loop"
    This reverts commit af6d4df20f5c4aabe90990582ac6bc6b3e774e16.

--- app/module/game.js
+++ app/module/game.js
@@ -3,11 +3,7 @@ angular.module('Game').constant('WHITE_PIECE', '\u26C0')
                       .constant('WHITE_KING', '\u26C1')
                       .constant('BLACK_PIECE', '\u26C2')
                       .constant('BLACK_KING', '\u26C3')
-                      .constant('BLACK_WINS', 1)
-                      .constant('WHITE_WINS', 2)
-                      .constant('NUM_PLAYERS', 2)
-angular.module('Game').service('Game', function(Grid, WHITE_PIECE, WHITE_KING, 
-   BLACK_PIECE, BLACK_KING, BOARDSIZE, BLACK_WINS, WHITE_WINS, NUM_PLAYERS) {
+angular.module('Game').service('Game', function(Grid, WHITE_PIECE, WHITE_KING, BLACK_PIECE, BLACK_KING, BOARDSIZE) {
    var ex = {}
      , that = {}
 
@@ -42,65 +38,14 @@ angular.module('Game').service('Game', function(Grid, WHITE_PIECE, WHITE_KING,
       });
    }
 
-   that.hasBlackPiece = function(square) {
-      return square.symbol === BLACK_PIECE;
-   }
-
-   that.hasWhitePiece = function(square) {
-      return square.symbol === WHITE_PIECE;
-   }
-
    ex.getGrid = Grid.getGrid;
 
    ex.startGame = function() {
       that.setUpPieces();
-      that.players = [{
-         name: 'Player 1',
-      },{
-         name: 'Player 2'
-      }];
-      that.turn = 0;
-   }
-
-   that.gameStatus = function() {
-      var whitePieces = 0, blackPieces = 0
-      Grid.forEach(function(square){
-         if (that.hasBlackPiece(square)) {
-            blackPieces++;
-         }
-         if (that.hasWhitePiece(square)) {
-            whitePieces++;
-         }
-      });
-      if (blackPieces===0) {
-         return WHITE_WINS;
-      } else if( whitePieces === 0){
-         return BLACK_WINS;
-      }
-      return false;
-   }
-
-   that.makeMove = function(player, cb) {
-      
-   }
-
-   ex.gameLoop = function(roundOverCB, gameOverCB) {
-      var gameStatus = that.gameStatus();
-      if (gameStatus===WHITE_WINS || gameStatus === BLACK_WINS) {
-         gameOverCB(gameStatus);
-         return this;
-      } else {
-         var player = ex.whoseTurn;
-         that.makeMove( player, function(move) {
-            that.turn++;
-            roundOverCB();
-            ex.gameLoop();
-         });
-      }
-   }
-
-   ex.whoseTurn = function() {
-      return angular.copy(that.players[ that.turn % NUM_PLAYERS ]);
+      // set up pieces
+      // wait next player to pick a move
+      // figure out result of move
+      // if game is not over, repeat this step and prior two.
    }
 
    return ex;
